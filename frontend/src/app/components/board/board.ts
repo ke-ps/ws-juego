ae5862552aad3f9375ac3679d1be848cd2886d4d
ke-ps <perezkevin166.kp@gmail.com>
2026-06-18 20:19:24 +0200

    component board

--- frontend/src/app/components/board/board.ts
+++ frontend/src/app/components/board/board.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { WebsocketService } from '../../services/websocket';
+import { ServerMessage } from '../../core/websocket.types';
+
+const ROWS = 6;
+const COLS = 7;
+
+function emptyBoard(): string[][] {
+  return Array.from({ length: ROWS }, () => Array(COLS).fill(''));
+}
 
 @Component({
   selector: 'app-board',
+  standalone: true,
   imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
-export class Board {}
+export class Board {
+  private readonly ws = inject(WebsocketService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  protected readonly board = signal<string[][]>(emptyBoard());
+
+  constructor() {
+    this.ws.connect();
+
+    this.ws
+      .messages()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((msg: ServerMessage) => {
+        this.handleMessage(msg);
+      });
+  }
+
+  private handleMessage(msg: ServerMessage): void {
+    switch (msg.type) {
+      case 'game_start':
+        if (msg.data['board']) {
+          this.board.set(msg.data['board'] as string[][]);
+        }
+        break;
+
+      case 'move':
+        this.board.set(msg.data.board);
+        break;
+
+      case 'game_over':
+        this.board.set(msg.data.board);
+        break;
+    }
+  }
+}
